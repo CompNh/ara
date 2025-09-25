5d41f3b0acc6f2eb4838eaad860f4cefe315e933
CompNh <saramanda21c@naver.com>
2025-09-25 12:05:45 +0900

    feat(tokens): scaffold @ara/tokens package (tsconfig + src + readme)

---
+++ packages/tokens/README.md
@@ -0,0 +1,3 @@
+# @ara/tokens
+
+Ara design tokens (Tier-0 scaffold). Real tokens will be added after Tier-0.
---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,5 @@
+/**
+ * @package @ara/tokens
+ * Tier-0: placeholder export (will be replaced by real design tokens)
+ */
+export const ARA_TOKENS_HELLO = '👋 Ara tokens';
