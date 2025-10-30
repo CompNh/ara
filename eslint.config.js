e8aad5552c984a26d63816da8a9535b6fd9b3984
CompNh <saramanda21c@naver.com>
2025-10-30 09:42:23 +0900

    fix: ignore markdown in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,6 @@ import { configs } from "@ara/eslint-config";
 export default [
   ...configs.node,
   {
-    files: ["**/*.md"],
-    languageOptions: {
-      parser: null
-    }
+    ignores: ["**/*.md"]
   }
 ];
