0bd497f0dc70b152aff495e80da07dcfa727f3df
CompNh <saramanda21c@naver.com>
2025-10-30 15:10:20 +0900

    feat(icons): initialize icons package (#61)
    WBS: W-000003
    Task: T-000020
    Task Name : 아이콘 패키지 초기화(packages/icons)
    
    ## Summary
    - 아이콘 패키지 스캐폴딩과 빌드 구성을 추가했습니다.
    - createIcon 유틸과 Arrow Right 샘플 아이콘을 정의했습니다.
    - 패키지 README와 트리 셰이킹 가능한 exports를 구성했습니다.
    
    ## Details
    - packages/icons 패키지 설정, tsconfig, eslint 구성을 추가했습니다.
    - IconDefinition 타입과 createIcon 팩토리를 구현했습니다.
    - arrowRight 아이콘과 index 집합 export를 제공했습니다.
    
    ## Breaking Changes
    - none
    
    ## References
    -

---
+++ packages/icons/src/index.ts
@@ -0,0 +1,9 @@
+import { arrowRight } from "./icons/arrow-right.js";
+
+export type { IconDefinition, IconName } from "./types.js";
+export { createIcon } from "./create-icon.js";
+export { arrowRight };
+
+export const icons = {
+  arrowRight,
+} as const;
