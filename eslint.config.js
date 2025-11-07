1df30f97d6c7db13df3dcd2cfebf6a8a04e95800
CompNh <saramanda21c@naver.com>
2025-11-07 14:06:35 +0900

    fix: register jsx-a11y plugin for react linting (#116)

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,17 @@
 import { configs } from "@ara/eslint-config";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+
+const REACT_FILE_PATTERNS = ["packages/react/**/*.{js,jsx,ts,tsx}"];
 
 export default [
   ...configs.node,
   {
     ignores: ["**/*.md"]
+  },
+  {
+    files: REACT_FILE_PATTERNS,
+    plugins: {
+      "jsx-a11y": jsxA11y
+    }
   }
 ];
