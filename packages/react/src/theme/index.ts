86364204bef5341e7afb813ad0d1ac7b7d103a05
CompNh <saramanda21c@naver.com>
2025-11-10 15:53:10 +0900

    [T-000041] ThemeProvider 산출물 정리

--- packages/react/src/theme/index.ts
+++ packages/react/src/theme/index.ts
@@ -8,3 +8,7 @@ export {
   useAraThemeVariableTable,
   useAraThemeVariables
 } from "./AraProvider.js";
+export {
+  ThemeProvider,
+  type ThemeProviderProps
+} from "../components/theme-provider/index.js";
