25e4a1d2aead12c9b2c087af7c952ae195ce5f72
CompNh <saramanda21c@naver.com>
2025-12-05 12:46:57 +0900

    [T-000107] React inert 속성 타입 추가

---
+++ packages/core/src/react-inert.d.ts
@@ -0,0 +1,11 @@
+import "react";
+
+declare module "react" {
+  interface HTMLAttributes<T> {
+    /**
+     * Global `inert` attribute support for browsers that implement it.
+     * Accepts boolean or string forms used by React (e.g., "", "true").
+     */
+    inert?: boolean | "" | "true";
+  }
+}
